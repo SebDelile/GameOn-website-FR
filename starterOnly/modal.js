955b6966aa7a310a7e404d9190ce5dab1c3a3294
Delile Sebastien <sebastiendelile@gmail.com>
2021-01-19 20:39:53 +0100

    begin review SCSS
    début de revue de tout les code CSS dans les fichier scss. Modif du html quand besoin
    revu : base, typo, button, container, header, footer, modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,7 +8,7 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalbg = document.querySelector(".blur");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
